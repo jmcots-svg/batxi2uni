4d3bf6fdf6a9b96da91876c9f9f1073e38dbd2e3
jmcots-svg <jmcots@gmail.com>
2026-03-03 11:27:11 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -55,7 +55,7 @@ Deno.serve(async (req) => {
       }));
 
       const geminiResponse = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${token}`,
+        `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key=${token}`,
         {
           method: "POST",
           headers: {
