97fd22e7ba0bc5925ab630e3cce036880462b000
jmcots-svg <jmcots@gmail.com>
2026-03-09 00:30:23 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -148,7 +148,7 @@ async function callGeminiWithFallback(
           ],
          tools: [
           { urlContext: {} },
-          { googleSearch: {}},
+          //{ googleSearch: {}},
         ]
         }
       });
